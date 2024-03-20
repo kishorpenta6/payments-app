fa7543104dd2f646e10046db42eeae9133949ab9
kishorpenta6 <155563274+kishorpenta6@users.noreply.github.com>
2024-03-20 19:17:11 +0530

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -2,11 +2,27 @@
 public class Wallet {
 	
 	private int UserId;
+	private double balance;
+	private double walletAmountLimit =10000;
 	
-	private int balance;
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getWalletAmountLimit() {
+		return walletAmountLimit;
+	}
+	public void setWalletAmountLimit(double walletAmountLimit) {
+		this.walletAmountLimit = walletAmountLimit;
+	}
 	
-	private double deposit;
-	private double withdraw;
-
 	
 }
